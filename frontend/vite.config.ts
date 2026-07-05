d1cce622d155bfa42929366bcfe1580335389be4
aymen-elo <aymen.elouagouti@gmail.com>
2026-07-05 08:18:23 +0200

    semi-fix gle vercel issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from "vite"
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api/integrations/acquisition": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+      },
     },
   },
 })
